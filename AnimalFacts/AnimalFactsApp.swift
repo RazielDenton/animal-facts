12ac360209a5aa794f5d1a85b175de8e14c62aee
Viacheslav Shulha <raziel.denton@icloud.com>
2024-07-11 16:51:54 +0300

    Create SwiftUI template with TCA & Realm packages

---
+++ AnimalFacts/AnimalFactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimalFactsApp.swift
+//  AnimalFacts
+//
+//  Created by Viacheslav on 11.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct AnimalFactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
